bcf0cd0e0e5e3ed3012e6b5974f71f2aa22f231a
Darmin Reyes <119169294+Darmin-RC@users.noreply.github.com>
2025-02-02 17:34:06 -0400

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,6 +1,18 @@
 from rest_framework import viewsets
 from .models import Proyecto, Tarea, Etiqueta, Comentario
-from .serializers import ProyectoSerializer, TareaSerializer, EtiquetaSerializer, ComentarioSerializer
+from .serializers import ProyectoSerializer, TareaSerializer, EtiquetaSerializer, ComentarioSerializer, UserSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+
+class UserDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
 class ProyectoViewSet(viewsets.ModelViewSet):
     queryset = Proyecto.objects.all()
