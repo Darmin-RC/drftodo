a66c50179e223530ca094b6149dc92b539310a16
Darmin Reyes <119169294+Darmin-RC@users.noreply.github.com>
2025-02-02 16:06:49 -0400

    Update wsgi.py

--- todo/wsgi.py
+++ todo/wsgi.py
@@ -1,8 +1,17 @@
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.contrib.auth import get_user_model
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo.settings')
+
+# Inicializa Django
+django.setup()
+
+# Crea un superusuario si no existe
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "darminrc@gmail.com", "D@rmin06")
+    print("Superusuario creado exitosamente.")
 
 application = get_wsgi_application()
